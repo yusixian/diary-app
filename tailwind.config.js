f121de65b83dc40a88a8d48a138937254fa6db8b
cos <cosine_yu@qq.com>
2024-12-01 14:52:50 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  darkMode: 'class', // https://tailwindcss.com/docs/dark-mode
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/hooks/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      screens: {
+        xs: { max: '475px' },
+        md: { max: '768px' },
+        tablet: '640px',
+        xl: '1366px',
+        '2xl': '1366px',
+      },
+      colors: {
+        primary: 'var(--primary)',
+        accent: {
+          default: 'var(--accent-100)',
+          200: 'var(--accent-200)',
+        },
+        danger: 'var(--danger)',
+        header: 'var(--bg-header)',
+        text: {
+          100: 'var(--text-100)',
+          200: 'var(--text-200)',
+        },
+        bg: {
+          100: 'var(--bg-100)',
+          200: 'var(--bg-200)',
+          300: 'var(--bg-300)',
+          900: 'var(--bg-900)',
+        },
+      },
+      backgroundImage: {
+        gradient: 'var(--gradient-bg)',
+        'gradient-pink': 'var(--gradient-pink)',
+      },
+      fontFamily: {
+        poppins: 'var(--font-poppins)',
+      },
+    },
+  },
+  plugins: [],
+};
